93ccca362aba64b7d83be983067a41609b16ea66
Joe <boat232001@yahoo.com>
2023-01-26 14:43:18 +1000

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,8 +11,8 @@ def do_pack():
     name = "web_static_{}{}{}{}{}{}.tgz".format(n.year, n.month,
                                                 n.day, n.hour,
                                                 n.minute, n.second)
-    local('mkdir -p versions')
-    local("tar -cvzf versions/{} web_static --mode=664 ".format(name))
+    local('sudo mkdir -p versions')
+    local("sudo tar -cvzf versions/{} web_static --mode=664 ".format(name))
     size = os.stat("versions/{}".format(name)).st_size  
     print("web_static packed: versions/{} -> {}Bytes".format(name, size))
 
